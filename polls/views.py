f54e16a6a2a7b4e4bf1e793d4db18963fb55d494
Tony Vazquez <90466229+joseantoniovazquez@users.noreply.github.com>
2023-03-16 19:54:33 -0600

    Tutorial 03

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from polls.models import Question
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world, You're at the polls index.")
+    # output=', '.join([q.question_text for q in latest_question_list])
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:10]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question = get_object_or_404(Question, pk=question_id)
+    except Question.DoesNotExist as e:
+        raise Http404("Question does not exist") from e
+    # return HttpResponse(f"You're looking at question {question_id}.")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"results from question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"you are voting un question {question_id}")
